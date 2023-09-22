3925cbe45c5aec87b0fdd48556d1025c994767dc
pablo8090 <37598740+pablo8090@users.noreply.github.com>
2023-09-22 04:18:10 +0200

    initial commit, spring project

---
+++ src/main/java/com/fltron/server/FlTronServerApplication.java
@@ -0,0 +1,13 @@
+package com.fltron.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlTronServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlTronServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fltron/server/FlTronServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fltron.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlTronServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
